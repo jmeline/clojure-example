ab3ae720eb96485d57599f6e1ef0c3fa1bca8652
Jacob Meline <jacob.meline@gmail.com>
2017-07-13 09:33:48 -0600

    Adding wallhaven-scraper

---
+++ wallhaven-scraper/test/wallhaven_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns wallhaven-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [wallhaven-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
