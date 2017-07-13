964eb9145bc49d815c492eabc5ab48b289a14ed9
Jacob Meline <jacob.meline@gmail.com>
2017-07-13 09:31:02 -0600

    cleaning up rnf max

--- recursive_nameless_functions/src/recursive_nameless_functions/core.clj
+++ recursive_nameless_functions/src/recursive_nameless_functions/core.clj
@@ -1,17 +1,20 @@
 (ns recursive-nameless-functions.core
+  (:require [recursive-nameless-functions.fib :as f])
   (:gen-class))
 
 (use 'recursive-nameless-functions.fractorial)
 
-(defn log [msg func]
-  (println (str msg (func))))
+(defn sample [f]
+  (map f (range 1 10)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
-  (log "(fractorial 19): " #(fractorial 19))
-  (log "((fractorial-gen fractorial) 19): " #((fractorial-gen fractorial) 19))
-  (log "((fractorial-weird fractorial-weird) 19): " #((fractorial-weird fractorial-weird) 19))
-  (log "(fractorial-no-names 19): " #(fractorial-no-names 19)))
+  (println "Hello, World!" )
+  (println "normal fib:"  (sample f/fib))
+  (println "fib-gen :"    (sample (f/fib-gen f/fib)))
+  (println "fib-weird :"  (sample (f/fib-weird f/fib-weird)))
+  (println "fib-nameless :"  (sample f/fib-nameless )))
+
+
 
