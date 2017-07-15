3327ccaa4e367526a2dd87cce6e55ccc2e0e0323
Jacob Meline <jacob.meline@gmail.com>
2017-07-15 14:55:31 -0600

    Adding 10 more elementary challenges for 4 clojure

--- clojure/challenges/test/challenges/4clojure/4clojure_elementary_test.clj
+++ clojure/challenges/test/challenges/4clojure/4clojure_elementary_test.clj
@@ -1,5 +1,7 @@
 (ns challenges.4clojure.4clojure_elementary_test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.set :refer :all]))
+
 
 ;; #1
 (deftest nothing-but-the-truth
@@ -39,6 +41,76 @@
         [1 2 3 4]
         (conj [1 2 3] 4)
         (conj [1 2] 3 4))))
+;; #8
+(deftest sets
+  (is (=
+       #{:a :b :c :d}
+       (set '(:a :a :b :c :c :c :d :d))
+       (clojure.set/union #{:a :b :c} #{:b :c :d}))))
+
+;; #9
+(deftest sets-conj
+  (is (=
+       #{1 2 3 4}
+       (conj #{1 4 3} 2))))
+
+;; #10
+(deftest intro-to-maps
+  (is (=
+       ((hash-map :a 10 :b 20 :c 30) :b)
+       (:b {:a 10 :b 20 :c 30}))))
+
+;; #11
+(deftest maps-conj
+  (is (=
+       {:a 1 :b 2 :c 3}
+       (conj {:a 1} {:b 2} [:c 3]))))
+
+;; #12
+(deftest intro-to-sequences
+  (is (=
+       (first '(3 2 1))
+       (second [2 3 4])
+       (last (list 1 2 3))
+       3)))
+
+;; #13
+(deftest sequences-rest
+  (is (=
+       [20 30 40]
+       (rest [10 20 30 40]))))
+
+;; #14
+(deftest functions-intro
+  (is (=
+       8
+       ((fn add-five [x] (+ x 5)) 3)
+       ((fn [x] (+ x 5)) 3)
+       (#(+ % 5) 3)
+       ((partial + 5) 3))))
+
+;; #15
+(deftest double-down
+  (let [f (fn [x] (* x 2))]
+    (is (=
+         (= (f 2) 4)
+         (= (f 3) 6)
+         (= (f 11) 22)
+         (= (f 7) 14)))))
+
+;; #16
+(deftest hello-world
+  (let [f #(str "Hello, " % "!")]
+   (is (=
+        (= (f "Dave") "Hello, Dave!")
+        (= (f "Jenn") "Hello, Jenn!")
+        (= (f "Rhea") "Hello, Rhea!")))))
+
+;; #17
+(deftest sequences-map
+ (is (=
+        [6 7 8]
+        (map #(+ % 5) '(1 2 3)))))
 
 ;; #72
 (deftest rearranging-code->>
