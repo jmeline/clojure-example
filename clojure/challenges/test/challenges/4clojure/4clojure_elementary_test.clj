1ea760568d6970153d3158f9840fae21ae93b6c5
Jacob Meline <jacob.meline@gmail.com>
2017-07-15 22:22:22 -0600

    changing 'is' to use 'are' if testing more than one case

--- clojure/challenges/test/challenges/4clojure/4clojure_elementary_test.clj
+++ clojure/challenges/test/challenges/4clojure/4clojure_elementary_test.clj
@@ -2,7 +2,6 @@
   (:require [clojure.test :refer :all]
             [clojure.set :refer :all]))
 
-
 ;; #1
 (deftest nothing-but-the-truth
   (is (= true true)))
@@ -17,36 +16,31 @@
 
 ;; #4
 (deftest intro-to-lists
-  (is (list :a :b :c)
-      '(:a :b :c)))
+  (is (list :a :b :c) '(:a :b :c)))
 
 ;; #5
 (deftest lists-conj
-  (is (=
-       [1 2 3 4]
+  (are [x] (= [1 2 3 4] x)
        (conj '(2 3 4) 1)
-       (conj '(3 4) 2 1))))
+       (conj '(3 4) 2 1)))
 
 ;; #6
 (deftest intro-to-vectors
-  (is (=
-       [:a :b :c]
+  (are [x] (= [:a :b :c] x)
        (list :a :b :c)
        (vec '(:a :b :c))
-       (vector :a :b :c))))
+       (vector :a :b :c)))
 
 ;; #7
 (deftest vectors-conj
-  (is (=
-        [1 2 3 4]
-        (conj [1 2 3] 4)
-        (conj [1 2] 3 4))))
+  (are [x] (= [1 2 3 4] x)
+       (conj [1 2 3] 4)
+       (conj [1 2] 3 4)))
 ;; #8
 (deftest sets
-  (is (=
-       #{:a :b :c :d}
+  (are [x] (= #{:a :b :c :d} x)
        (set '(:a :a :b :c :c :c :d :d))
-       (clojure.set/union #{:a :b :c} #{:b :c :d}))))
+       (clojure.set/union #{:a :b :c} #{:b :c :d})))
 
 ;; #9
 (deftest sets-conj
@@ -68,70 +62,80 @@
 
 ;; #12
 (deftest intro-to-sequences
-  (is (=
+  (are [x] (= x 3)
        (first '(3 2 1))
        (second [2 3 4])
-       (last (list 1 2 3))
-       3)))
+       (last (list 1 2 3))))
 
 ;; #13
 (deftest sequences-rest
-  (is (=
-       [20 30 40]
-       (rest [10 20 30 40]))))
+  (is (= [20 30 40] (rest [10 20 30 40]))))
 
 ;; #14
 (deftest functions-intro
-  (is (=
-       8
+  (are [x] (= 8 x)
        ((fn add-five [x] (+ x 5)) 3)
        ((fn [x] (+ x 5)) 3)
        (#(+ % 5) 3)
-       ((partial + 5) 3))))
+       ((partial + 5) 3)))
 
 ;; #15
 (deftest double-down
   (let [f (fn [x] (* x 2))]
-    (is (=
-         (= (f 2) 4)
-         (= (f 3) 6)
-         (= (f 11) 22)
-         (= (f 7) 14)))))
+    (are [x y] (= (f x) y)
+         2 4
+         3 6
+         11 22
+         7 14)))
 
 ;; #16
 (deftest hello-world
   (let [f #(str "Hello, " % "!")]
-   (is (=
-        (= (f "Dave") "Hello, Dave!")
-        (= (f "Jenn") "Hello, Jenn!")
-        (= (f "Rhea") "Hello, Rhea!")))))
+    (are [x y] (= x y)
+         (f "Dave") "Hello, Dave!"
+         (f "Jenn") "Hello, Jenn!"
+         (f "Rhea") "Hello, Rhea!")))
 
 ;; #17
 (deftest sequences-map
- (is (=
-        [6 7 8]
-        (map #(+ % 5) '(1 2 3)))))
+  (is (= [6 7 8] (map #(+ % 5) '(1 2 3)))))
 
 ;; #18
 (deftest sequences-filter
- (is (=
-        [6 7]
-        (filter #(> % 5) '(3 4 5 6 7)))))
+  (is (= [6 7] (filter #(> % 5) '(3 4 5 6 7)))))
 
 ;; #35
 (deftest local-bindings
- (is (=
-      (= (let [x 5] (+ 2 x)) 7)
-      (= (let [x 3 y 10] (- y x)) 7)
-      (= (let [x 21] (let [y 3] (/ x y))) 7))))
+  (are [x] (= 7 x)
+       (let [x 5] (+ 2 x))
+       (let [x 3 y 10] (- y x))
+       (let [x 21] (let [y 3] (/ x y)))))
+
+;; #36
+(deftest let-it-be
+  (are [x y] (= x y)
+       10 (let [x 7 y 3] (+ x y))
+       4  (let [y 3 z 1] (+ y z))
+       1  (let [z 1] z)))
+
+;; #37
+(deftest regular-expressions
+  (is (= "ABC" (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))))
+
+;; #64
+(deftest intro-to-reduce
+  (are [x y] (= x y)
+       15 (reduce + [1 2 3 4 5])
+       0  (reduce + [])
+       6  (reduce + 1 [2 3])))
 
 ;; #72
 (deftest rearranging-code->>
-  (is (= (reduce + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
-         (->>
-           [2 5 4 1 3 6]
-           (drop 2)
-           (take 3)
-           (map inc)
-           (reduce +))
-         11)))
+  (are [x] (= 11 x)
+       (reduce + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+       (->>
+         [2 5 4 1 3 6]
+         (drop 2)
+         (take 3)
+         (map inc)
+         (reduce +))))
