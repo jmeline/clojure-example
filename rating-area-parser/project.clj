6f17c611551e5a2b7b8613219f8835a3119cbf92
Jacob Meline <jacob.meline@gmail.com>
2017-09-15 22:27:33 -0600

    Adding rating area parser work in progress

---
+++ rating-area-parser/project.clj
@@ -0,0 +1,11 @@
+(defproject rating-area-parser "0.0.1"
+  :description "A Simple application that parses rating areas from cms"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]
+                 [http-kit "2.2.0"]]
+  :main ^:skip-aot rating-area-parser.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ rating-area-parser/src/rating_area_parser/core.clj
@@ -0,0 +1,27 @@
+(ns rating-area-parser.core
+  (:require [net.cgrand.enlive-html :as html]
+            [org.httpkit.client :as http])
+  (:gen-class))
+
+(defn get-abbr-url [abbr]
+  (str "https://www.cms.gov/CCIIO/Programs-and-Initiatives/Health-Insurance-Market-Reforms/" abbr "-gra.html"))
+
+(def states ["AK" "AL" "AR" "AZ" "CA" "CO" "CT" "DE" "FL"
+             "GA" "HI" "IA" "ID" "IL" "IN" "KS" "KY" "LA"
+             "MA" "MD" "ME" "MI" "MN" "MO" "MS" "MT" "NC"
+             "ND" "NE" "NH" "NJ" "NM" "NV" "NY" "OH" "OK"
+             "OR" "PA" "RI" "SC" "SD" "TN" "TX" "UT" "VA"
+             "VT" "WA" "WI" "WV" "WY"])
+
+(defn get-dom [state]
+  (html/html-snippet
+   (:body @ (http/get (get-abbr-url state)))))
+
+(defn get-rows [dom]
+  (html/select dom [:div.list-bullet :tr :td :p]))
+
+(defn -main
+  "My attempt at writing a rating-area parser in clojure"
+  [& args]
+  ;; (doseq [state (last states)] (println (get-rows (get-dom state))))
+  (println (get-rows (get-dom (last states)))))
