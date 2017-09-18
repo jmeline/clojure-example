a793b57924ceafcc0226604b0d8333786f1f97ea
Jacob Meline <jacob.meline@gmail.com>
2017-09-17 20:44:02 -0600

    Renaming functions for the greater good

--- rating-area-parser/src/rating_area_parser/core.clj
+++ rating-area-parser/src/rating_area_parser/core.clj
@@ -3,7 +3,7 @@
             [org.httpkit.client :as http])
   (:gen-class))
 
-(defn get-abbr-url [abbr]
+(defn get-url [abbr]
   (str "https://www.cms.gov/CCIIO/Programs-and-Initiatives/Health-Insurance-Market-Reforms/" abbr "-gra.html"))
 
 (def states ["AK" "AL" "AR" "AZ" "CA" "CO" "CT" "DE" "FL"
@@ -13,19 +13,25 @@
              "OR" "PA" "RI" "SC" "SD" "TN" "TX" "UT" "VA"
              "VT" "WA" "WI" "WV" "WY"])
 
-(defn get-dom [state]
+(defn get-state-specific-dom [state]
   (html/html-snippet
-   (:body @(http/get (get-abbr-url state)))))
+   (:body @(http/get (get-url state)))))
 
-(defn get-rows [dom]
+(defn get-table-rows [dom]
   (html/select dom [:div.list-bullet :tr :td :p]))
 
-(defn get-main-content [state]
-  (get-rows (get-dom state)))
+(defn get-table-rows-from-dom [state]
+  (-> state 
+      (get-state-specific-dom)
+      (get-table-rows)))
 
-(defn -main
+(defn partition-rating-areas [state]
+  (->> state
+       (get-table-rows-from-dom)
+       (drop 3)
+       (map #(apply str (:content %)))
+       (partition 3)))
+
+(defn -main [& args]
   "My attempt at writing a rating-area parser in clojure"
-  [& args]
-  (let [rows (drop 3 (get-main-content (last states)))]
-    (partition 3 (map #(apply str (:content %)) rows))))
-
+  (partition-rating-areas (last states)))
