fb0fc7f97498539efe00e7e1fb55a27cf5e7e3d3
Jacob Meline <jacob.meline@gmail.com>
2017-09-23 17:56:07 -0600

    Adding a few tests

--- rating-area-parser/test/rating_area_parser/core_test.clj
+++ rating-area-parser/test/rating_area_parser/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [rating-area-parser.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest rating-area-parser-testing
+  (testing "Gets correct URL"
+    (let [result "https://www.cms.gov/CCIIO/Programs-and-Initiatives/Health-Insurance-Market-Reforms/WA-gra.html"]
+    (is (= (get-url "WA") result))))
+
+  (testing "county-name-format"
+    (is (= (county-name-format "Rating Area 1" "Cache" []) ["Rating Area 1" "Cache" " "]))
+    (is (= (county-name-format "Rating Area 2" "Clark") ["Rating Area 2" "Clark" " "])))
+
+  (testing "three-digit-zip-format"
+    (is (= (three-digit-zip-format "Rating Area 2" "995" []) ["Rating Area 2" " " "995"]))
+    (is (= (three-digit-zip-format "Rating Area 3" "445") ["Rating Area 3" " " "445"]))))
