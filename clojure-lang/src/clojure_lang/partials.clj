8a2706e7639b91fabe02a1d6d17f5a31dc02c103
Jacob Meline <jacob.meline@gmail.com>
2018-03-06 23:25:26 -0700

    fixing my own partial function. Added multiple tests around it

--- clojure-lang/src/clojure_lang/partials.clj
+++ clojure-lang/src/clojure_lang/partials.clj
@@ -1,7 +1,9 @@
 (ns clojure-lang.partials)
 
 ;; define my own partial
+;; Returns a function provided N-1 args that asks for more arguments.
 (defn my-partial
   [partialized-fn & args]
   (fn [& more-args]
-    (apply partialized-fn (into args more-args))))
+    (apply partialized-fn
+           (concat args more-args))))
--- clojure-lang/test/clojure_lang/partial_test.clj
+++ clojure-lang/test/clojure_lang/partial_test.clj
@@ -1,7 +1,22 @@
 (ns clojure-lang.partial-test
-  (:require  [clojure.test :as t]
-             [clojure-lang.partial :as p]))
+  (:require  [clojure.test :refer :all]
+             [clojure-lang.partials :as p]))
 
-(t/deftest
-  (def add20 (p/my-partial + 20))
-  (t/is (= (add20 3) 23)))
+(def add20 (p/my-partial + 20))
+
+(deftest partial-add20
+  (are [x y] (= x y)
+    (add20 3) 23
+    (add20 4) 24
+    (add20) 20))
+
+(deftest partial-customfn
+  (defn sampleFn [x y z]
+    (assoc {} :x x :y y :z z))
+  (def oneArg (p/my-partial sampleFn 5))
+  (def twoArgs (p/my-partial sampleFn 2 5))
+  (def threeArgs (p/my-partial sampleFn 2 5 3))
+  (are [m v] (= m v)
+    (twoArgs 9) {:x 2 :y 5 :z 9}
+    (oneArg 2 3) {:x 5 :y 2 :z 3}
+    (threeArgs) {:x 2 :y 5 :z 3}))
