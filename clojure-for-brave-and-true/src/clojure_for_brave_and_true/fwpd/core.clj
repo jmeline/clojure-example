a83f5c0795d830e79a17ac161f3fbd79ae992875
Jacob Meline <jacob.meline@gmail.com>
2018-03-14 08:51:28 -0600

    Adding fwpd exercises and tests

--- clojure-for-brave-and-true/src/clojure_for_brave_and_true/fwpd/core.clj
+++ clojure-for-brave-and-true/src/clojure_for_brave_and_true/fwpd/core.clj
@@ -51,3 +51,39 @@
 (defn filter-glitter-index
   [minimum-glitter records]
   (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(defn filter-glitter-index-by-name
+  ;; 1. Turn the result of your glitter filter into a list of names.
+  [minimum-glitter records]
+  (map :name (filter-glitter-index minimum-glitter records)))
+
+(defn append
+  ;; 2. Write a function, append, which will append a new suspect to your list of suspects.
+  [new-suspect suspects]
+  (cons new-suspect suspects))
+
+(defn validate
+  ;; 3. Write a function, validate, which will check that :name and :glitter-index
+  ;; are present when you append. The validate function should accept two arguments:
+  ;; a map of keywords to validating functions, similar to conversions, and the record
+  ;; to be validated.
+  [validations record]
+  (let [validations-results
+        (reduce
+         (fn [acc [key val :as all]]
+           (assoc acc key
+                  (if-let [validation-fn (key validations)]
+                    ((key validations) val)
+                    true))) {} record)]
+    (assoc record :validations validations-results)))
+
+(defn map->csv
+  ;; 4. Write a function that will take your list of maps and convert it back to a CSV string.
+  ;; You’ll need to use the clojure.string/join function.
+  [records]
+  (let [to-list
+        (fn [record]
+          (clojure.string/join "," (reduce
+                                    (fn [acc [key val]]
+                                      (conj acc (name key) val)) [] record)))]
+    (map to-list records)))
--- clojure-for-brave-and-true/test/clojure_for_brave_and_true/fwpd/core_test.clj
+++ clojure-for-brave-and-true/test/clojure_for_brave_and_true/fwpd/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure-for-brave-and-true.fwpd.core :as sut]
             [clojure.test :refer :all]))
 
+(def sampleData
+  '({:name "Edward Cullen", :glitter-index 10}
+    {:name "Bella Swan", :glitter-index 0}
+    {:name "Charlie Swan", :glitter-index 0}
+    {:name "Jacob Black", :glitter-index 3}
+    {:name "Carlisle Cullen", :glitter-index 6}))
+
 (deftest fwpd-tests
   (testing "convert"
     (is (= (sut/convert :name "jacob") "jacob"))
@@ -15,16 +22,28 @@
     (is (= (sut/convert-key-list-into-map '([:name "Edward Cullen"] [:glitter-index "15"]))
            {:name "Edward Cullen" :glitter-index 15})))
   (testing "filtering based on glitter index"
-    (def sampleData
-      '({:name "Edward Cullen", :glitter-index 10}
-        {:name "Bella Swan", :glitter-index 0}
-        {:name "Charlie Swan", :glitter-index 0}
-        {:name "Jacob Black", :glitter-index 3}
-        {:name "Carlisle Cullen", :glitter-index 6}))
     (is (= (sut/filter-glitter-index 3 sampleData)
-           '({:name "Edward Cullen", :glitter-index 10}
-             {:name "Jacob Black", :glitter-index 3}
-             {:name "Carlisle Cullen", :glitter-index 6})))
+           '({:name "Edward Cullen" :glitter-index 10}
+             {:name "Jacob Black" :glitter-index 3}
+             {:name "Carlisle Cullen" :glitter-index 6})))
     (is (= (sut/filter-glitter-index 7 sampleData)
-           '({:name "Edward Cullen", :glitter-index 10})))
-    (is (= (sut/filter-glitter-index 11 sampleData) '()))))
+           '({:name "Edward Cullen" :glitter-index 10})))
+    (is (= (sut/filter-glitter-index 11 sampleData) '())))
+  (testing "additional exercises"
+    (testing "filter-glitter-index-by-name"
+      (is (= (sut/filter-glitter-index-by-name 3 sampleData)
+             '("Edward Cullen" "Jacob Black" "Carlisle Cullen")))
+      (is (= (sut/filter-glitter-index-by-name 10 sampleData)
+             '("Edward Cullen"))))
+    (testing "append"
+      (let [updatedSampleData
+            (sut/append {:name "Voldermort" :glitter-index 11} sampleData)]
+        (is (= (first (sut/filter-glitter-index 11 updatedSampleData))
+               {:name "Voldermort" :glitter-index 11}))))
+    (testing "validate"
+      (let [validations {:name (fn [x] (> (count x) 0))
+                         :glitter-index (fn [x] (> x 0))}]
+        (is (= (sut/validate validations (first sampleData))
+               {:name "Edward Cullen" :glitter-index 10 :validations {:name true :glitter-index true}}))))
+    (testing "map->csv"
+      (is (= (sut/map->csv sampleData))))))
