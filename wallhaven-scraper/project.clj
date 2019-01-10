02848696c51d915eab3dd4076d10138a7ca4ec38
Jacob Meline <jacob.meline@gmail.com>
2019-01-09 22:41:17 -0700

    Updating the project to use enlive and httpkit instead of clj-http

--- wallhaven-scraper/project.clj
+++ wallhaven-scraper/project.clj
@@ -1,10 +1,11 @@
 (defproject wallhaven-scraper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Scraping the website wallhaven.cc for wallpapers"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.6.1"]]
+  :license {:name "MIT"
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [enlive "1.1.6"]
+                 [http-kit "2.3.0"]]
   :main ^:skip-aot wallhaven-scraper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
